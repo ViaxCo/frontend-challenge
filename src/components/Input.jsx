910ecc7d2d5b082e1c4845f75d940670ad7b930e
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2023-04-30 07:58:56 +0100

    Add styles for modal

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Input = ({ label }) => {
+const Input = ({ label, styledClasses }) => {
   return (
     <div>
-      <h5>{label}</h5>
-      <input placeholder="Enter deposit amount" className="text-box"></input>
+      <h5 className="my-3 font-semibold text-base">{label}</h5>
+      <input
+        placeholder="Enter deposit amount"
+        className="text-box text-lato text-base border-gray"
+      ></input>
     </div>
   );
 };
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,18 +7,28 @@ const Modal = ({ action, setShowModal, userData }) => {
   const [openModal, setOpenModal] = useState(true);
   return (
     <div className="modal">
-      <div className="">
-        <h4>
-          {action}
-          <span className='ml-1'>{action === 'Deposit' ? 'to' : 'from'}</span> wallet
-        </h4>
-        <CloseButton onClick={() => setShowModal(!openModal)} />
-      </div>
-      <div className="input-box">
-        <Input label="Amount" />
-      </div>
-      <div className="modal-button">
-        <button onClick={() => setShowModal(false)}>{action}</button>
+      <div className="w-95 mx-auto my-6">
+        <div className="flex justify-between items-center w-full  ">
+          <h4 className="text-2xl font-semibold">
+            {action}
+            <span className="ml-1">
+              {action === 'Deposit' ? 'to' : 'from'}
+            </span>{' '}
+            wallet
+          </h4>
+          <CloseButton onClick={() => setShowModal(!openModal)} />
+        </div>
+        <div className="input-box">
+          <Input className="w-full" label="Amount" />
+        </div>
+        <div className="modal-button">
+          <button
+            className="bg-deyork text-white text-base p-3 border-none rounded-sm"
+            onClick={() => setShowModal(false)}
+          >
+            {action}
+          </button>
+        </div>
       </div>
     </div>
   );
